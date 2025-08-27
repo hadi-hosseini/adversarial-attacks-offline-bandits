842fc76c888a6f05f6cff28f91da050c531fb5eb
hadi-hosseini <hadi.hosseini0171@gmail.com>
2025-08-27 08:28:28 +0000

    results done

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@ from typing import Tuple
 
 @dataclass
 class COFNIG:
-    k: int = 3 # The number of arms # k = [3, 5, 10]
-    d: int = 100  # The dimension of attack and mus # d = [10000, 1000, 100, 10] 
+    k: int = 4 # The number of arms # k = [3, 5, 10]
+    d: int = 1000  # The dimension of attack and mus # d = [10000, 1000, 100, 10] 
     n_samples: int = 3000  # The number of samples for each arm in the logged data 
     sigma: float = 1.0  # The variance of Gaussian distribution of each arm
     T: int = 100  # The number of rounds # T = [100, 50]
